20100243ff4688ef1638e8f07c1577e153ecc7ad
Engine Treasure <enginechen07@gmail.com>
2016-11-10 15:44:33 +0800

    add fn: read, long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,34 @@
+import os
 from setuptools import setup
+
+
+def read(frame):
+    return open(os.path.join(os.path.dirname(__file__), frame)).read()
+
+
+# def long_description(frame):
+#     import codecs
+#     with codecs.open(
+#             os.path.join(os.path.dirname(__file__), frame), encoding="utf-8"
+#     ) as f:
+#         return f.read()
+
 
 setup(
     name='Python-Project-Template',
-    version='0.1',
-    packages=['Project', 'Project.libs'],
-    url='https://github.com/Vic020/Python-Project-Template',
-    license='GNU v3',
+    version='0.0.1',
     author='VicYu',
     author_email='no@vicyu.com',
+    license='GNU v3',
     description='',
-    requires=['click']
+    # home page for the package
+    url='https://github.com/Vic020/Python-Project-Template',
+    packages=[
+        'Project',
+        'Project.libs'
+    ],
+    long_description=read("README"),
+    requires=[
+        'click'
+    ]
 )
